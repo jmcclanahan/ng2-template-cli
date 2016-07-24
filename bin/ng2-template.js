d40100553dba8ba7fbaac42774d8856b3993abad
Jakob McClanahan <jmcclanahan18@gmail.com>
2016-07-23 21:30:41 -0500

    Move templates to /bin and use commander

---
+++ bin/ng2-template.js
@@ -0,0 +1,118 @@
+#!/usr/bin/env node
+const fs       = require('fs')
+const path     = require('path')
+const mkdirp   = require('mkdirp')
+const template = require('lodash.template')
+const program  = require('commander')
+
+program
+  .version('1.0.1')
+  .description('Generate component templates based off of the Angular 2 style guide')
+  .option('-t, --type <type>', 'Define the type of component you want to generate(eg. component,service,etc)')
+  .option('-n, --name <name>', 'Give your component a name')
+  .option('-b, --root [root]', 'Define a new root path for template generation(the default is ./src/app/)')
+  .option('-p, --parent [parent]', 'Define a new relative path to generate your template at(this path is relative to ./src/app/)')
+  .parse(process.argv)
+
+if (!program.type || !program.name) {
+  console.log('The type and name of the component you want to generate is required. Type "ng2T --help" for an example.')
+  process.exit(1)
+}
+
+const parent   = program.parent || ''
+const _root    = root('src', 'app', parent)
+
+const COMPONENT = 'component'
+const DIRECTIVE = 'directive'
+const SERVICE   = 'service'
+const ENUM      = 'enum'
+const INTERFACE = 'interface'
+const PIPE      = 'pipe'
+
+// Templates
+const COMPONENT_TEMPLATE      = path.resolve(__dirname, 'temp.component.tpl')
+const COMPONENT_HTML_TEMPLATE = path.resolve(__dirname, 'temp.component.html.tpl')
+const COMPONENT_CSS_TEMPLATE  = path.resolve(__dirname, 'temp.component.css.tpl')
+const COMPONENT_SPEC_TEMPLATE = path.resolve(__dirname, 'temp.component.spec.tpl')
+const DIRECTIVE_TEMPLATE      = path.resolve(__dirname, 'temp.directive.tpl')
+const DIRECTIVE_SPEC_TEMPLATE = path.resolve(__dirname, 'temp.directive.spec.tpl')
+const SERVICE_TEMPLATE        = path.resolve(__dirname, 'temp.service.tpl')
+const SERVICE_SPEC_TEMPLATE   = path.resolve(__dirname, 'temp.service.spec.tpl')
+const ENUM_TEMPLATE           = path.resolve(__dirname, 'temp.enum.tpl')
+const INTERFACE_TEMPLATE      = path.resolve(__dirname, 'temp.tpl')
+const PIPE_TEMPLATE           = path.resolve(__dirname, 'temp.pipe.tpl')
+const PIPE_SPEC_TEMPLATE      = path.resolve(__dirname, 'temp.pipe.spec.tpl')
+
+// Files To Create
+const COMPONENT_FILE      = path.resolve(_root, program.name, `${program.name}.component.ts`)
+const COMPONENT_HTML_FILE = path.resolve(_root, program.name, `${program.name}.component.html`)
+const COMPONENT_CSS_FILE  = path.resolve(_root, program.name, `${program.name}.component.css`)
+const COMPONENT_SPEC_FILE = path.resolve(_root, program.name, `${program.name}.component.spec.ts`)
+const DIRECTIVE_FILE      = path.resolve(_root, `${program.name}.directive.ts`)
+const DIRECTIVE_SPEC_FILE = path.resolve(_root, `${program.name}.directive.spec.ts`)
+const SERVICE_FILE        = path.resolve(_root, `${program.name}.service.ts`)
+const SERVICE_SPEC_FILE   = path.resolve(_root, `${program.name}.service.spec.ts`)
+const ENUM_FILE           = path.resolve(_root, `${program.name}.enum.ts`)
+const INTERFACE_FILE      = path.resolve(_root, `${program.name}.ts`)
+const PIPE_FILE           = path.resolve(_root, `${program.name}.pipe.ts`)
+const PIPE_SPEC_FILE      = path.resolve(_root, `${program.name}.pipe.spec.ts`)
+
+const files = new Map()
+const options = {
+  'name': program.name,
+  'classifiedName': program.name.charAt(0).toUpperCase() + program.name.slice(1),
+  'interfacePrefix': 'I'
+}
+
+switch(program.type) {
+  case COMPONENT:
+    files.set(COMPONENT_TEMPLATE, COMPONENT_FILE)
+    files.set(COMPONENT_HTML_TEMPLATE, COMPONENT_HTML_FILE)
+    files.set(COMPONENT_CSS_TEMPLATE, COMPONENT_CSS_FILE)
+    files.set(COMPONENT_SPEC_TEMPLATE, COMPONENT_SPEC_FILE)
+    break
+  case DIRECTIVE:
+    files.set(DIRECTIVE_TEMPLATE, DIRECTIVE_FILE)
+    files.set(DIRECTIVE_SPEC_TEMPLATE, DIRECTIVE_SPEC_FILE)
+    break
+  case SERVICE:
+    files.set(SERVICE_TEMPLATE, SERVICE_FILE)
+    files.set(SERVICE_SPEC_TEMPLATE, SERVICE_SPEC_FILE)
+    break
+  case ENUM:
+    files.set(ENUM_TEMPLATE, ENUM_FILE)
+    break
+  case INTERFACE:
+    files.set(INTERFACE_TEMPLATE, INTERFACE_FILE)
+    break
+  case PIPE:
+    files.set(PIPE_TEMPLATE, PIPE_FILE)
+    files.set(PIPE_SPEC_TEMPLATE, PIPE_SPEC_FILE)
+    break
+  default:
+    break
+}
+
+createFiles()
+
+function createFiles() {
+  const dir = program.type === COMPONENT ? path.resolve(_root, program.name) : _root
+
+  mkdirp(dir, function(err) {
+    if (err) { throw err }
+
+    for (let [key, value] of files) {
+      fs.readFile(key, "utf8", function(err, data) {
+        if (err) { throw err }
+        const compiled = template(data)
+        fs.appendFile(value, compiled(options))
+      })
+    }
+  })
+  console.log('Success!')
+}
+
+function root(args) {
+  args = [...arguments] || ''
+  return path.join(process.cwd(), ...args)
+}
